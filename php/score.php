4390f6e0baa597deab4862020592ff7b3d782b0b
acmoua <54963439+acmoua@users.noreply.github.com>
2019-12-06 16:52:13 -0800

    score.php

---
+++ php/score.php
@@ -0,0 +1,16 @@
+<?php
+if($_POST["username"]){
+
+$uname = json_decode($_POST['username']);
+echo $uname;
+$gmode = json_decode($_POST['gamemode']);
+echo $gmode;
+$gridsize = json_decode($_POST['gridsize']);
+echo $gridsize;
+$globaltimer = json_decode($_POST['globaltimer']);
+echo $globaltimer;
+$p1score = json_decode($_POST['p1score']);
+echo $p1score;
+}
+
+?>
