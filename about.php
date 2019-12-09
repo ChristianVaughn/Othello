5878bcb55f6640109511654da4f69aae8ab4776a
acmoua <54963439+acmoua@users.noreply.github.com>
2019-12-08 16:05:26 -0800

    added basic about page

---
+++ about.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link href="./css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/howto.css">
+</head>
+<nav >
+  <ul>
+  <li><a href="leaderboard.php" >Home</a></li>
+    <li><a href="index.php" >Game</a></li>
+    <li><a href="howto.php" >How To Play</a></li>
+    <li><a class="active" href="about.php" >About</a></li>
+    <li style="float:right"><a href="php/logout.php" >Log Out</a></li>
+    <li style="float:right"><a href="php/reset-password.php">Account</a></li>
+  </ul>
+</nav>
+<body style="color:black;" >
+  <br>
+  <br>
+  <h2 >Purpose</h2>
+<button type="button" class="collapsible">Project</button>
+<div class="content">
+  <p  >The goal of this project is to demonstrate the skills and techniques we have while learned attending CSCI-130 (Web Programming ) during the 2019 fall semester at Fresno State. These skills will serve to be lifelong beneficial skills that will improve our chances of having a successful career in the Computer science field.
+  </p>
+</div>
+<h2 >Tools</h2>
+<button type="button" class="collapsible">Programming languages and tools</button>
+<div class="content">
+  <p >Tools we used in devemopment of this project</p>
+  <p>Tools:</p>
+  <li>Atom - free and open-source text and source code editor with support for plug-ins written in Node.js, and embedded Git Control, developed by GitHub.</li><br>
+  <li>VSCode - source-code editor. It includes support for debugging, embedded Git control and GitHub, syntax highlighting, intelligent code completion, snippets, and code refactoring. </li><br>
+  <p>Languages:</p>
+  <li>HTML - Hypertext Markup Language, the standard markup language for documents designed to be displayed in a web browser.</li>
+  <li>Javascript - a high-level, just-in-time compiled, object-oriented programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions. </li>
+  <li>PHP - a general-purpose programming language originally designed for web development.</li>
+</div>
+<h2 >About Us</h2>
+<button type="button" class="collapsible">Cvaughn</button>
+<div class="content">
+  <p>Education</p>
+  <li>stuff....</li><br>
+
+  <p>Programming Experience</p>
+  <li>stuff </li><br>
+  <li>stuff   </li><br>
+  <p>github</p>
+  <a href="">
+  <img src="https://bit.ly/2DZT7ZW" alt="HTML tutorial" style="width:150px;height:100px;border:0">
+  </a>
+</div>
+<button type="button" class="collapsible">Andym</button>
+<div style="color:black;" class="content">
+  <p>Education</p>
+  <li>AST in Computer Science</li><br>
+
+  <p>Programming Experience</p>
+  <li>C++  </li><br>
+  <p>github</p>
+  <a href="https://github.com/acmoua">
+  <img src="https://bit.ly/2DZT7ZW" alt="HTML tutorial" style="width:150px;height:100px;border:0">
+</a>
+</div>
+
+</body>
+<script src="js/howto.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</html>
